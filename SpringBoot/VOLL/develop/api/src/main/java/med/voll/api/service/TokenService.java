14695bb423c07367c6086cc8eb1684fabd971e32
luizbsadev <luiz.bsa.olv@gmail.com>
2023-09-13 12:28:53 -0300

    Criando Metodo getSubject na classe TokenService

--- SpringBoot/VOLL/develop/api/src/main/java/med/voll/api/service/TokenService.java
+++ SpringBoot/VOLL/develop/api/src/main/java/med/voll/api/service/TokenService.java
@@ -3,6 +3,7 @@ package med.voll.api.service;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import med.voll.api.entidade.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -28,6 +29,19 @@ public class TokenService {
         } catch (JWTCreationException exception){
             throw new RuntimeException("erro ao gerar token jwt", exception);
         }
+    }
+
+    public String getSubject(String jwt){
+        try{
+        var algoritmo = Algorithm.HMAC256(secret);
+        return JWT.require(algoritmo)
+                .withIssuer("API Voll.med")
+                .build()
+                .verify(jwt)
+                .getSubject();
+    } catch (JWTVerificationException exception) {
+        throw new RuntimeException("Token JWT inválido ou expirado!");
+    }
     }
 
     private Instant dataExpiracao() {
