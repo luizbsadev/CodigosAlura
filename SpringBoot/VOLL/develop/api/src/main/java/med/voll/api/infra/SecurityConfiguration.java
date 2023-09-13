48308a4570e3cc8bb779665e41eeede29c6773d0
luizbsadev <luiz.bsa.olv@gmail.com>
2023-09-13 12:30:17 -0300

    Alterando Metodos na classe SecurityConfiguration

--- SpringBoot/VOLL/develop/api/src/main/java/med/voll/api/infra/SecurityConfiguration.java
+++ SpringBoot/VOLL/develop/api/src/main/java/med/voll/api/infra/SecurityConfiguration.java
@@ -1,7 +1,9 @@
 package med.voll.api.infra;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,16 +12,29 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
 
+    @Autowired
+    SecurityFilter securityFilter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.csrf(csrf -> csrf.disable())
-                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .build();
+         http.csrf(csrf -> csrf.disable())
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+         http.authorizeHttpRequests(authorize -> {
+             authorize.requestMatchers(HttpMethod.POST, "/login").permitAll();
+             authorize.anyRequest().authenticated();
+         });
+
+
+         http.addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
+
+         return http.build();
     }
 
     @Bean
