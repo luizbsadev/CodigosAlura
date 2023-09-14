34296c3eaf47c83695ad907d38aee0acb3b66b00
luizbsadev <luiz.bsa.olv@gmail.com>
2023-09-14 14:41:08 -0300

    Criando query para receber um medico aleatorio caso o paciente não especifique um medico

--- SpringBoot/VOLL/develop/api/src/main/java/med/voll/api/repository/MedicoRepository.java
+++ SpringBoot/VOLL/develop/api/src/main/java/med/voll/api/repository/MedicoRepository.java
@@ -1,13 +1,33 @@
 package med.voll.api.repository;
 
+import med.voll.api.entidade.Especialidade;
 import med.voll.api.entidade.Medico;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface MedicoRepository extends JpaRepository<Medico, Long> {
 
     Page<Medico> findAllByAtivoTrue(Pageable paginacao);
+
+    @Query("""
+                select m from Medico m
+                where
+                m.ativo = true
+                and
+                m.especialidade = :especialidade
+                and
+                m.id not in(
+                        select c.medico.id from Consulta c
+                        where
+                        c.data = :data
+                )
+                order by rand()
+                limit 1
+                """)
+    Medico escolherMedicoAleatorioLivreNaDataPorEspecialidade(Especialidade especialidade, LocalDateTime data);
 }
