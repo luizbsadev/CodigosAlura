386815500f6aa45cb6d26fd65d5df893f6729b0a
luizbsadev <luiz.bsa.olv@gmail.com>
2023-09-14 14:35:05 -0300

    Renomeando DTO para ficar padronizado

--- SpringBoot/VOLL/develop/api/src/main/java/med/voll/api/dto/DadosMensagemErro400DTO.java
+++ SpringBoot/VOLL/develop/api/src/main/java/med/voll/api/dto/DadosMensagemErro400DTO.java
@@ -2,8 +2,8 @@ package med.voll.api.dto;
 
 import org.springframework.validation.FieldError;
 
-public record DadosMensagemErro400(String campo, String mensagem ) {
-    public DadosMensagemErro400(FieldError erro){
+public record DadosMensagemErro400DTO(String campo, String mensagem ) {
+    public DadosMensagemErro400DTO(FieldError erro){
         this(erro.getField(), erro.getDefaultMessage());
     }
 }
--- SpringBoot/VOLL/develop/api/src/main/java/med/voll/api/dto/DadosTokenJWT.java
+++
@@ -1,4 +0,0 @@
-package med.voll.api.dto;
-
-public record DadosTokenJWT(String token) {}
-
